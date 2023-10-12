edc450c833d77d6631d68ccc6dbb6464313b4d77
z vallarino <zvcoding@gmail.com>
2023-10-12 09:41:25 -0400

    working

--- tensor3.py
+++ tensor3.py
@@ -2,48 +2,68 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
-
-
+# print(tf.__version__)
+#
+#
 ## features
-X = np.array([-7.0,-4.0,-1.0,2.0,5.0, 8.0, 11.0, 14.0])
+X = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
 
 
 
 ##labels
 
-y = np.array([3.0,6.0,9.0,12.0,15.0,18.0,21.0,24.0])
+y = np.array([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
 
-X = tf.constant(X)
-y = tf.constant(y)
-
-print(X,y)
-plt.scatter(X,y)
-
-plt.show()
+X = tf.range(-100, 100, 4)
 
 
+y = X + 10
 
-input_shape = X[0].shape
-output_shape = y[0].shape
-print(input_shape,output_shape)
+# print(X,y)
+# plt.scatter(X,y)
+#
+# plt.show()
+#
+#
+#
+# input_shape = X[0].shape
+# output_shape = y[0].shape
+# print(input_shape,output_shape)
+#
+# tf.random.set_seed(42)
+#
+# ## Create a model using the Sequential Api
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(1)
+# ])
+#
+# ## Complie the model
+# model.compile(loss=tf.keras.losses.mae,
+#               optimizer=tf.keras.optimizers.SGD(),
+#               metrics=["mae"])
+#
+# ##Fit the model
+# model.fit(tf.expand_dims(X,axis=-1),y,epochs=100)
+#
+#
+# print(model.predict([17.0]))
 
-tf.random.set_seed(42)
-
-## Create a model using the Sequential Api
+# 1. Create the model
 model = tf.keras.Sequential([
+    tf.keras.layers.Dense(100, activation="relu"),
     tf.keras.layers.Dense(1)
 ])
 
-## Complie the model
+# 2. Complie the model
 model.compile(loss=tf.keras.losses.mae,
-              optimizer=tf.keras.optimizers.SGD(),
-              metrics=["mae"])
+              optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+              metrics=['mae'])
 
-##Fit the model
-model.fit(tf.expand_dims(X,axis=-1),y,epochs=50)
+#3. Fit the model(this time we will train for longer)
+model.fit(tf.expand_dims(X,axis=-1),y, epochs=200)
 
-print(X)
-print(y)
+plt.scatter(X,y)
+plt.show()
 
 print(model.predict([17.0]))
+
